e57935ccc7d969957c62ffeb9dd50103c2b8fae8
S Roze <sherozem4@gmail.com>
2021-11-01 17:34:48 +0000

    utilised arrow functions and other functions, understanding differences. All code is contained in the script.js file, and previous exercises are commented out for ease of continuation onto next topic

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,5 +55,21 @@ const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
 
-const age2 = calcAge2(1997);
-console.log(age1, age2);
+// const age2 = calcAge2(1997);
+// console.log(age1, age2);
+
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1994);
+console.log(age3);
+
+
+const yearsToRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsToRetirement(2001, `Sheroze`));

d44e5e97ad6d9fbdb1863875e54158789aade8d4
S Roze <sherozem4@gmail.com>
2021-11-01 14:44:57 +0000

    Understood functions, and the ability to distinguish function declaration and function expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -3,12 +3,57 @@
 // Strict mode forbids us to do certain things
 // It creates visible errors in the console
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log(`i can drive`);
+// if(passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log(`i can drive`);
 
 
 // const interface = "Audio";
 // const private = 534;
+
+
+
+
+// function logger() { 
+//     console.log(`My name is Sheroze`);
+// }
+
+
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) { 
+
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
+
+
+const age1 = calcAge1(1997);
+
+// Function declaration
+function calcAge1(birthYear) { 
+    return 2037 - birthYear;
+}
+
+
+console.log(age1);
+
+
+// Function expression
+const calcAge2 = function (birthYear) { 
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1997);
+console.log(age1, age2);

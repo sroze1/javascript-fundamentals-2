3f7dfcad0ffaf0422ccb12bd765bfb100c3cb010
S Roze <sherozem4@gmail.com>
2021-11-01 11:23:58 +0000

    all starter files have been added for ease of use when updating and pushing changes to git

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,37 @@
+true;
+
+let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+
+
+javascriptIsFun = 'Yes';
+//  This allows us to redefine our variable without having to add a new value using let
+
+console.log(typeof javascriptIsFun);
+
+let year;
+// console.log(year);
+// console.log(typeof year);
+
+
+// year = 1991;
+
+// console.log(year);
+// console.log(typeof year +  " " + year);
+
+// console.log(typeof null);
+
+let isIsland = false;
+let language;
+let population = 229000000;
+let country = "Pakistan";
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+

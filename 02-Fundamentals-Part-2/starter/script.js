6942d92d0e49a6961108718b150071225e20cfe7
S Roze <sherozem4@gmail.com>
2021-11-09 12:38:30 +0000

    Arrays functionality completed. Short exercise to understand implementations and uses. Will be continued further

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -129,17 +129,66 @@
 // DATA 2: DOLPHINS SCORE 85, 54, 41 ---- KOALAS SCORE 23, 34 AND 27
 
 
-const calcAverage =  (score1, score2, score3) =>(score1 + score2 + score3) / 3;
+// const calcAverage =  (score1, score2, score3) =>(score1 + score2 + score3) / 3;
 
-const averageScoreDolph = calcAverage(85, 54, 41);
-const averageScoreKoala = calcAverage(23,34,27);
+// const averageScoreDolph = calcAverage(85, 54, 41);
+// const averageScoreKoala = calcAverage(23,34,27);
 
-function checkWinner(average1, average2) { 
-        if(average1 >=2* average2) { 
-                console.log(`Dolphins win with an average of ${average1}`)
-            } else if (average2 >=2* average1) { 
-                console.log(`Koalas win with an average of ${average2}`)
-            } else console.log(`No team wins!`)
-    }
-checkWinner(averageScoreDolph, averageScoreKoala);
+// function checkWinner(average1, average2) { 
+//         if(average1 >=2* average2) { 
+//                 console.log(`Dolphins win with an average of ${average1}`)
+//             } else if (average2 >=2* average1) { 
+//                 console.log(`Koalas win with an average of ${average2}`)
+//             } else console.log(`No team wins!`)
+//     }
+// checkWinner(averageScoreDolph, averageScoreKoala);
 
+const friend1 = `Michael`;
+const friend2 = `Steven`;
+const friend3 = `Peter`;
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+
+
+
+
+console.log(friends[0]);
+
+
+console.log(friends.length);
+console.log(friends[friends.length-1]);
+
+
+friends[2] = `Jay`;
+console.log(friends);
+
+// friends= [`Bob`, `Alice`];
+
+
+const firstName = `Jonas`;
+const jonas = [firstName, `Sheroze`, 2037 - 1997, friends];
+
+console.log(jonas)
+
+
+
+// Exercise
+const age1 = calcAge1(1997);
+
+// Function declaration
+function calcAge1(birthYear) { 
+    return 2037 - birthYear;
+}
+
+const years= [1990, 1967, 2002, 2010, 2018];
+calcAge1(years);
+
+
+const age11 = calcAge1(years[0]);
+const age2 = calcAge1(years[1]);
+const age3 = calcAge1(years[years.length - 1]);
+
+const ages = [calcAge1(years[0]), calcAge1(years[1]), calcAge1(years[years.length - 1])];
+console.log(ages);

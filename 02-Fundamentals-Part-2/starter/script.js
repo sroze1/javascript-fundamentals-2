7e1384204e45bf7acbd1fc115cba498205982426
S Roze <sherozem4@gmail.com>
2021-11-11 11:53:36 +0000

    Brackets notation explored further,to utilise user inputs in the prompt in browser. The value is the returned to the prompt variable, and this is matched and replaced to extract the property value of the array

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -279,10 +279,10 @@ const jonas = {
     friends: ['Michael', 'Peter', 'Steven']
 }
 
-console.log(jonas);
+// console.log(jonas);
 
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
 
 const nameKey = 'Name';
 console.log(jonas['first' + nameKey]);
@@ -290,4 +290,21 @@ console.log(jonas['first' + nameKey]);
 
 const interestedIn = prompt(`What do you want to know about Jonas? Choose between
 firstName, lastName, age, job, and friends`);
-console.log(jonas[interestedIn]);
+
+
+if(jonas[interestedIn]) { 
+    console.log(jonas[interestedIn]);
+} else { 
+    console.log(`Wrong request, please choose between firstName, lastName, age, job and friends`);
+} console.log(jonas);
+
+jonas.location = 'Portgual';
+jonas['twitter'] = '@jonaslogan';
+console.log(jonas);
+
+
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friends is called ${jonas.friends[0]}.`);
